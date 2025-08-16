19db7cf4d192240dac06acba769f11129e811cf8
hexane <ethanefem@proton.me>
2025-08-16 03:14:07 +0200

    Update vite.config.js for compatibility outsideof

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import restart from 'vite-plugin-restart'
 export default {
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets
-    base: '/darmfest2000/', // 👈 IMPORTANT: repo name here for GH Pages
+    base: '/', // 👈 IMPORTANT: repo name here for GH Pages
     server: {
         host: true,
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
