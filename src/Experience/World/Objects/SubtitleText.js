a1a457de026186b046628778c2ebb15557e18fa6
hexane <ethanefem@proton.me>
2025-08-16 02:16:24 +0200

    Adjusted for even better mobile readability 2

--- src/Experience/World/Objects/SubtitleText.js
+++ src/Experience/World/Objects/SubtitleText.js
@@ -91,7 +91,7 @@ export default class SubitleText
     {
         if(this.sizes.aspectRatio < 1.8) // Offset Z to make sure text fits on screen, if aspect ratio is less than 1.8 text would be cropped
         {
-            this.Zoffset = (1 - this.sizes.aspectRatio / 1.8) ** 2 * 120 // Adjust the Z offset based on aspect ratio
+            this.Zoffset = (1 - this.sizes.aspectRatio / 1.8) ** 2 * 150 // Adjust the Z offset based on aspect ratio
             console.log(this.Zoffset)
             this.position.z = -20 - this.Zoffset
         } else
